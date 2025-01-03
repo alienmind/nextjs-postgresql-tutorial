3a3e5a021c1e95fee049838dc464624c2bb72200
alienmind <alienmind@gmail.com>
2025-01-03 20:34:35 +0100

    Chapter 13 completed

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -30,10 +30,16 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
-  connectionPool.query(`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES ('${customerId}', ${amountInCents}, '${status}', '${date}')
-  `);
+  try {
+    connectionPool.query(`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES ('${customerId}', ${amountInCents}, '${status}', '${date}')
+    `);
+  } catch (error) {
+    return {
+        message : "Database Error: Failed to Create Invoice.',"
+    };
+  }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
@@ -51,18 +57,28 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   const amountInCents = amount * 100;
  
-  connectionPool.query(`
-    UPDATE invoices
-    SET customer_id = '${customerId}', amount = ${amountInCents}, status = '${status}'
-    WHERE id = '${id}'
-  `);
- 
+  try {
+    connectionPool.query(`
+      UPDATE invoices
+      SET customer_id = '${customerId}', amount = ${amountInCents}, status = '${status}'
+      WHERE id = '${id}'
+    `);
+  } catch (error) {
+    return { message : "Database Error: Failed to Update Invoice." };
+  } 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-  connectionPool.query(`DELETE FROM invoices WHERE id = '${id}'`);
-  revalidatePath('/dashboard/invoices');
+  throw new Error('Failed to Delete Invoice');
+  try {
+    connectionPool.query(`DELETE FROM invoices WHERE id = '${id}'`);
+    revalidatePath('/dashboard/invoices');
+  } catch (error) {
+    return {
+      message : "Database Error: Failed to Delete Invoice."
+    };
+  }
 }
  
