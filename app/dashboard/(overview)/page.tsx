61e53c0d262c62e1030444402e82fbf95a586ab3
alienmind <alienmind@gmail.com>
2025-01-04 00:32:52 +0100

    Completed course. Also added .env.example

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,6 +6,11 @@ import { Suspense } from 'react';
 import { LatestInvoicesSkeleton,
          RevenueChartSkeleton,
          CardSkeleton } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
  
 export default async function Page() {
   return (
